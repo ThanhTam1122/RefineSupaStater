acfea4aea69f3efab8326620d940a393d1f29ed7
tam <thanhtam215019@gmail.com>
2024-05-24 22:53:32 +0900

    feat: init refine project

---
+++ src/utility/supabase-client.ts
@@ -0,0 +1,17 @@
+import { createClient } from '@refinedev/supabase'
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+  throw new Error('Supabase URL and Key must be defined')
+}
+
+export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  db: {
+    schema: 'public',
+  },
+  auth: {
+    persistSession: true,
+  },
+})
